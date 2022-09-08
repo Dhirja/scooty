fbbdf6666c9667912fb7dac902b25d779b4da16b
dhirajdubey19965@gmail.com <dhirajdubey19965@gmail.com>
2022-09-08 23:11:31 +0530

    added

--- scooty-rental/rentalscooty/src/App.js
+++ scooty-rental/rentalscooty/src/App.js
@@ -1,16 +1,23 @@
+
+import './App.css';
 import {Home} from "./Scooty/Home"
+import Signup from './Scooty/Signup';
+import { Login } from './Scooty/Login';
 import {Route, Routes} from "react-router-dom";
 import { Display } from './Scooty/Display';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-       <Routes>
+           
+     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/dis/:id' element={<Display/>}/>
-       
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Signup/>}/>
       </Routes>
+         
     </div>
   );
 }
---
+++ scooty-rental/rentalscooty/src/Scooty/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, theme,Box,Text,VStack,Input,Button,Link } from '@chakra-ui/react'
+
+
+
+export const Login = () => {
+  return (
+    <Container >
+    <Box borderWidth="1px" borderRadius="lg" p={4} mt={10}>
+        <Text fontSize="2xl" align="center">Create Acount</Text>
+        <Text fontSize="md" align="center" pb={4}>Create an account? <Link color="teal.500" href='/register'>Create Account</Link></Text>
+        <form >
+        <VStack spacing={2}
+        align="stretch"
+        pb={3}
+        >
+            <Input placeholder="Username" name='username'/>
+            <Input  placeholder="Password" name='password'/>
+            
+        </VStack>
+        <Button  colorScheme="teal" variant="solid" >Log In </Button>
+        </form>
+    </Box>
+</Container>
+  )
+}
