c7611bc52af6692daa468ecf4497dc0e8c88efd7
dhirajdubey19965@gmail.com <dhirajdubey19965@gmail.com>
2022-09-08 22:15:37 +0530

    addaed

--- scooty-rental/rentalscooty/src/App.js
+++ scooty-rental/rentalscooty/src/App.js
@@ -1,6 +1,5 @@
 import {Home} from "./Scooty/Home"
-import Signup from './Scooty/Signup';
-import { Login } from './Scooty/Login';
+
 import {Route, Routes} from "react-router-dom";
 import { Display } from './Scooty/Display';
 import './App.css';
@@ -11,8 +10,7 @@ function App() {
        <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/dis/:id' element={<Display/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Signup/>}/>
+      
       </Routes>
     </div>
   );
--- scooty-rental/rentalscooty/src/index.js
+++ scooty-rental/rentalscooty/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom"
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+
+<React.StrictMode>
+    <BrowserRouter>
+  
+     
+        <App />
+    
+  
+    </BrowserRouter>
   </React.StrictMode>
-  </BrowserRouter>
-);
